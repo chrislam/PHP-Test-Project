e3ddfaabfc6645da42696bad283eac893d780f35
Chris Lam <chris2trees@gmail.com>
2014-07-23 22:00:23 +1200

    Make login page pretty

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,7 +1,40 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test Project</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+    <!-- Customs styles -->
+    <link rel="stylesheet" href="/css/styles.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
   <body>
-    <h1>Gladeye Test Project</h1>
-    <div>@yield('sub-heading', 'by Chris Lam')</div>
-    <div>@yield('content')</div>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Test Project</a>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container">
+      @yield('content')
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   </body>
 </html>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,22 +1,25 @@
 @extends('layout')
 
 @section('content')
-  {{ Form::open(array('action' => 'LoginController@doLogin' )) }}
-  <div>
-    <?php
-      echo Form::label('email', 'Email');
-      echo Form::email('email');
-    ?>
+  <h2>Login</h2>
+  {{ Form::open(array('action' => 'LoginController@doLogin', 'class' => 'form-horizontal')) }}
+  <div class="form-group">
+    {{ Form::label('email', 'Email', $attributes = array('class' => 'col-sm-2 control-label')) }}
+    <div class="col-sm-4">
+      {{ Form::email('email', null, $attributes = array('class' => 'form-control')) }}
+    </div>
   </div>
-  <div>
-    <?php
-      echo Form::label('password', 'Password');
-      echo Form::password('password');
-    ?>
+  <div class="form-group">
+    {{ Form::label('password', 'Password', $attributes = array('class' => 'col-sm-2 control-label')) }}
+    <div class="col-sm-4">
+      {{ Form::password('password', $attributes = array('class' => 'form-control')) }}
+    </div>
   </div>
-  <div>
-    <?php echo Form::submit('Login'); ?>
-    <?php echo link_to_action('RegistrationController@showRegister', 'Register', $parameters = array(), $attributes = array()); ?>
+  <div class="form-group">
+    <div class="col-sm-10 col-sm-offset-2">
+      {{ Form::submit('Login', $attributes = array('class' => 'btn btn-primary')); }}
+      {{ link_to_action('RegistrationController@showRegister', 'Register', $parameters = array(), $attributes = array('class' => 'btn btn-default')); }}
+    </div>
   </div>
   {{ Form::close() }}
 @stop
